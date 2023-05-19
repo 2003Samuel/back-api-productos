1480a82fdb6c3cfd330331969421f1cf35ac57a9
2003Samuel <samuelmo332@gmail.com>
2023-05-19 17:36:32 -0500

    Project full

---
+++ app/Http/Controllers/ProductosController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ProductosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $productos = Producto::all();
+        return response()->json($productos);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $producto = new Producto();
+        $producto->codigo = $request->codigo;
+        $producto->nombre = $request->nombre;
+        $producto->cantidad = $request->cantidad;
+        $producto->precio = $request->precio;
+        $producto->save();
+
+        return response()->json($producto);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $producto = Producto::find($id);
+        return response()->json($producto);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $producto = Producto::find($id);
+
+        if (is_null($producto)) {
+            return response()->json('No se encontro el id del producto');
+        } else {
+            $producto->codigo = $request->codigo;
+            $producto->nombre = $request->nombre;
+            $producto->cantidad = $request->cantidad;
+            $producto->precio = $request->precio;
+            $producto->save();
+
+            return response()->json($producto);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $producto = Producto::find($id);
+        if (is_null($producto)) {
+            return response()->json('No se encontro el producto');
+        } else {
+            $producto->delete();
+            return response()->json($producto);
+        }
+    }
+}
